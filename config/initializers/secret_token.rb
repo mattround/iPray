9a77299237a61c0e48c290e57d2aff94842c69fe
Matt Round <mround@clearlybrilliant.com>
2012-06-07 20:54:14 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ipray::Application.config.secret_token = 'dec6239b78f8acd7b7eefa8896c0e78ef4ca6aacbb30f1e64e5a071f7aa022b914b7ea511539f3592f7c58e1d0004e254a59f152bba9759d1b2cfd37e371dab8'
